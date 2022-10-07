fd0962de38ce92899805fa45374745491bf59612
Angel Jain <interdexeloper@gmail.com>
2022-10-07 16:33:24 +0530

    Final

---
+++ src/components/Data.js
@@ -0,0 +1,14 @@
+export const countries = [
+  {
+    image: "/images/daji.jpg",
+    title: "Daji",
+  },
+  {
+    image: "/images/hall1.jpg",
+    title: "Hall",
+  },
+  {
+    image: "/images/hall2.jpg",
+    title: "Another Hall",
+  }
+];
